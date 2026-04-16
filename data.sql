b6e7cf7527577fcbfcb8ce03e935ceb7beec5105
seefinho <instaseef@gmail.com>
2026-04-16 19:03:14 +0100

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,23 @@
+INSERT INTO customers VALUES
+(101, 'Ahmed', 'London'),
+(102, 'Sarah', 'Leeds'),
+(103, 'Omar', 'Manchester'),
+(104, 'Aisha', 'Birmingham'),
+(105, 'Khalid', 'Bristol');
+
+INSERT INTO products VALUES
+(1, 'Mint Shisha', 'Shisha'),
+(2, 'Latte', 'Drinks'),
+(3, 'Grape Shisha', 'Shisha'),
+(4, 'Mojito', 'Drinks'),
+(5, 'Tea', 'Drinks');
+
+INSERT INTO orders VALUES
+(1, 101, 1, '2026-04-10', 18),
+(2, 102, 2, '2026-04-10', 6),
+(3, 101, 3, '2026-04-10', 20),
+(4, 103, 4, '2026-04-10', 25),
+(5, 104, 5, '2026-04-11', 8),
+(6, 101, 4, '2026-04-11', 12),
+(7, 105, 2, '2026-04-11', 10),
+(8, 102, 1, '2026-04-12', 18);
